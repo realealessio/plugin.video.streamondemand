fa5e5bf1e62b56ff98d95748aed3da355593d991
Kevin <gtatruth11943@gmail.com>
2017-05-07 04:42:25 +0200

    New Channels & Disabled Server

---
+++ channels/italiafilm01.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# streamondemand.- XBMC Plugin
+# Canale per http://italiafilm01.co/
+# http://www.mimediacenter.info/foro/viewforum.php?f=36
+# By MrTruth
+# ------------------------------------------------------------
+
+import re
+
+from core import logger
+from core import config
+from core import servertools
+from core import scrapertools
+from core.item import Item
+from core.tmdb import infoSod
+
+__channel__ = "italiafilm01"
+__category__ = "F,A"
+__type__ = "generic"
+__title__ = "ItaliaFilm01"
+__language__ = "IT"
+
+host = "http://italiafilm01.co"
+
+headers = [
+    ['User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'],
+    ['Accept-Encoding', 'gzip, deflate'],
+    ['Referer', host]
+]
+
+def isGeneric():
+    return True
+
+# ----------------------------------------------------------------------------------------------------------------
+def mainlist(item):
+    logger.info("[ItaliaFilm01.py]==> mainlist")
+    itemlist = [Item(channel=__channel__,
+                     action="peliculas",
+                     title=color("Film", "azure"),
+                     url="%s/film" % host,
+                     thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
+                Item(channel=__channel__,
+                     action="peliculas",
+                     title=color("Film in HD", "azure"),
+                     url="%s/film-streaming-hd" % host,
+                     thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
+                Item(channel=__channel__,
+                     action="peliculas",
+                     title=color("Animazione", "azure"),
+                     url="%s/film/animazione" % host,
+                     thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
+                Item(channel=__channel__,
+                     action="search",
+                     title=color("Cerca ...", "yellow"),
+                     thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search")]
+
+    return itemlist
+
+# ================================================================================================================
+
+# ----------------------------------------------------------------------------------------------------------------
+def search(item, texto):
+    logger.info("[ItaliaFilm01.py]==> search")
+    item.url = host + "/cerca/film?q=" + texto
+    try:
+        return peliculas(item)
+    # Se captura la excepción, para no interrumpir al buscador global si un canal falla
+    except:
+        import sys
+        for line in sys.exc_info():
+            logger.error("%s" % line)
+        return []
+
+# ================================================================================================================
+
+# ----------------------------------------------------------------------------------------------------------------
+def peliculas(item):
+    logger.info("[ItaliaFilm01.py]==> peliculas")
+    itemlist = []
+
+    data = scrapertools.anti_cloudflare(item.url, headers=headers)
+    patron = r'<a href="([^"]+)".*?>\s*(?:<div class="item\-film\-img">\s*|)<img src="([^"]+)" alt="([^"]+)">'
+    matches = re.compile(patron, re.DOTALL).findall(data)
+
+    for scrapedurl, scrapedthumbnail, scrapedtitle in matches:
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
+        scrapedtitle = re.sub(r'(?:C|c)over', '', scrapedtitle).strip()
+        itemlist.append(infoSod(
+            Item(channel=__channel__,
+                 action="findvideos",
+                 contentType="movie",
+                 title=scrapedtitle,
+                 fulltitle=scrapedtitle,
+                 url=scrapedurl,
+                 extra="movie",
+                 thumbnail=scrapedthumbnail,
+                 folder=True), tipo="movie"))
+
+    # Pagine
+    patron = r'<a href="([^"]+)" rel="next">'
+    matches = re.compile(patron, re.DOTALL).findall(data)
+
+    if len(matches) > 0:
+        scrapedurl = matches[0]
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="HomePage",
+                 title="[COLOR yellow]Torna Home[/COLOR]",
+                 folder=True)),
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="peliculas",
+                 title="[COLOR orange]Successivo >>[/COLOR]",
+                 url=scrapedurl,
+                 thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
+                 extra=item.extra,
+                 folder=True))
+
+    return itemlist
+
+# ================================================================================================================
+
+# ----------------------------------------------------------------------------------------------------------------
+def findvideos(item):
+    logger.info("[ItaliaFilm01.py]==> findvideos")
+    data = scrapertools.anti_cloudflare(item.url, headers)
+    
+    itemlist = servertools.find_video_items(data=data)
+
+    for videoitem in itemlist:
+        server = re.sub(r'[-\[\]\s]+', '', videoitem.title)
+        videoitem.title = "".join(["[%s] Trailer: " % color(server, 'orange'), item.title])
+        videoitem.fulltitle = item.fulltitle
+        videoitem.show = item.show
+        videoitem.thumbnail = item.thumbnail
+        videoitem.channel = __channel__
+
+    videourl = scrapertools.find_single_match(data, r'<source src="([^"]+)"').strip()
+    itemlist.append(
+        Item(channel=__channel__,
+             action="play",
+             title="[%s] %s" % (color("Diretto .mp4", "orange"), item.title),
+             url=videourl,
+             thumbnail=item.thumbnail,
+             fulltitle=item.fulltitle,
+             show=item.fulltitle))
+    return itemlist
+
+# ================================================================================================================
+
+# ----------------------------------------------------------------------------------------------------------------
+def color(text, color):
+    return "[COLOR "+color+"]"+text+"[/COLOR]"
+    
+def HomePage(item):
+    import xbmc
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand/)")
+
+# ================================================================================================================
