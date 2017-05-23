b8c33dc0972a1966c50fa2888963582cefa4aa24
Kevin <gtatruth11943@gmail.com>
2017-05-23 04:25:03 +0200

    Small Fix & Fix Domain

--- channels/guardaserieonline.py
+++ channels/guardaserieonline.py
@@ -39,7 +39,7 @@ def mainlist(item):
     logger.info("[GuardaSerieOnline.py]==> mainlist")
     itemlist = [Item(channel=__channel__,
                      action="nuoveserie",
-                     title=color("Nuove serie TV Aggiunte", "orange"),
+                     title=color("Nuove serie TV", "orange"),
                      url=host,
                      thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
                 Item(channel=__channel__,
@@ -191,24 +191,6 @@ def lista_serie(item):
                  extra=item.extra,
                  show=scrapedtitle,
                  folder=True), tipo="tv"))
-    
-    patron = '<a\s*class="nextpostslink" rel="next" href="([^"]+)">'
-    matches = re.compile(patron, re.DOTALL).findall(data)
-
-    if len(matches) > 0:
-        scrapedurl = matches[0]
-        itemlist.append(
-            Item(channel=__channel__,
-                 action="HomePage",
-                 title=color("Torna Home", "yellow"),
-                 folder=True))
-        itemlist.append(
-            Item(channel=__channel__,
-                action="lista_serie",
-                title=color("Successivo >>", "orange"),
-                url=scrapedurl,
-                thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
-                folder=True))
     return itemlist
 
 # ================================================================================================================
